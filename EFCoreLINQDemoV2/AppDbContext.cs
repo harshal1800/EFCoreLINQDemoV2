47ebea44152993fdb6c4c1c6c7f57fd91a7c3c95
harshal1800 <harshalchavhank18@gmail.com>
2025-12-26 15:22:27 +0530

    Add project files.

---
+++ EFCoreLINQDemoV2/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder
+            .UseSqlServer("Server=localhost;Database=EFCoreDemoDB;Trusted_Connection=True;TrustServerCertificate=True;")
+            .LogTo(Console.WriteLine, LogLevel.Information);
+    }
+}
---
+++ EFCoreLINQDemoV2/Program.cs
@@ -0,0 +1,29 @@
+﻿//using (var context = new AppDbContext())
+//{
+//    var products = context.Products.ToList();
+
+//    foreach (var product in products)
+//    {
+//        Console.WriteLine($"{product.Id} - {product.Name} - {product.Price}");
+//    }
+//}
+
+//Console.WriteLine("LINQ Query Executed Successfully!");
+//Console.ReadLine();
+
+
+using (var context = new AppDbContext())
+{
+    var products = context.Products
+                          .Where(p => p.Price > 1000)
+                          .OrderBy(p => p.Name)
+                          .ToList();
+
+    foreach (var product in products)
+    {
+        Console.WriteLine($"{product.Id} - {product.Name} - {product.Price}");
+    }
+}
+
+Console.WriteLine("Filtered LINQ Query Executed Successfully!");
+Console.ReadLine();
